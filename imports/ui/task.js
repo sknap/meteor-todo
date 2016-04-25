46a4f23b6ae2ae11510637fecad51a585f9ffffc
Simon Knapp <simon.knapp@usma.edu>
2016-04-25 12:36:05 -0400

    V1.0

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -4,6 +4,13 @@ import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
  
 import './task.html';
+
+Template.task.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
+  },
+});
+
  
 Template.task.events({
   'click .toggle-checked'() {
@@ -12,5 +19,8 @@ Template.task.events({
   },
   'click .delete'() {
     Meteor.call('tasks.remove', this._id);
+  },
+   'click .toggle-private'() {
+    Meteor.call('tasks.setPrivate', this._id, !this.private);
   },
 });
